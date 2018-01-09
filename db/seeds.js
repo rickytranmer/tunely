bd4b865e1016d54ed57fb8ffbc18f411e71fe279
rickytranmer <rwtranmer@gmail.com>
2018-01-09 16:52:57 -0700

    splits genres

---
+++ db/seeds.js
@@ -0,0 +1,17 @@
+/*  Approximate schema for these seeds
+var SongSchema = new Schema({
+  name: String,
+  trackNumber: Number,
+});
+*/
+
+
+var sampleSongs = [];
+
+sampleSongs.push({name: 'Famous', 				trackNumber: 1});
+sampleSongs.push({name: "All of the Lights",	trackNumber: 2});
+sampleSongs.push({name: 'Guilt Trip', 			trackNumber: 3});
+sampleSongs.push({name: 'Paranoid', 			trackNumber: 4});
+sampleSongs.push({name: 'Ultralight Beam', 		trackNumber: 5});
+sampleSongs.push({name: 'Runaway', 				trackNumber: 6});
+sampleSongs.push({name: 'Stronger', 			trackNumber: 7});
